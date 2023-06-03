d7fdc4000b3c15df3d27cd638c8c5bd5368d2661
leesuyeon <lgh0385hh@gmail.com>
2023-06-03 22:08:33 +0900

    finith

--- src/componenets/Expenses/Expenses.js
+++ src/componenets/Expenses/Expenses.js
@@ -1,48 +1,37 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFliter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
+  // 년도를 Select 하면 Filter 하는 기능
+  const filteredExpenses = props.items.filter(expense => {
+    // getFullYear() : date에서 4자리수 연도를 가져옴
+    // toString() : 문자열로 반환
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+
+        
   return (
     <div>
       <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem 
-          title={expense.title} 
-          amount = {expense.amount} 
-          date = {expense.date} />)
-          )};
-        
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter selected={filteredYear} 
+          onChangeFilter={filterChangeHandler} 
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses} />        
+    
+
       </Card>
     </div>
   );
